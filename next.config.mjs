f1f61c474fa11620a030500e2f5a3acb300ac235
@yiek <ekue.ayi@insuco.com>
2025-06-27 09:31:01 +0000

    feat:configure eslint, prettier and sharp

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+    remotePatterns:[
+      {
+        //TODO: add hostname for google drive
+        hostname: "",
+      }
+    ]
   },
 }
 
