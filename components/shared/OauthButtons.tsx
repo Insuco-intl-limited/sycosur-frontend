532e19a5779842ea71301fbcd3ec0b78012f0fef
@yiek <ekue.ayi@insuco.com>
2025-11-04 19:28:32 +0000

    refactor: réorganisation de la structure des fichiers et ajout des pages de gestion des utilisateurs
    - Correction du nom de fichier OauthButttons.tsx -> OauthButtons.tsx
    
    - Ajout des pages de gestion des utilisateurs web et mobile
    
    - Suppression de l'ancien onglet SettingsTab non utilisé

--- components/shared/OauthButtons.tsx
+++ components/shared/OauthButtons.tsx
@@ -2,15 +2,15 @@
 
 import { GoogleLogin } from "@/utils";
 import OauthButton from "@/components/shared/OauthButtton";
-import {GoogleIcon} from "@/components/shared/GoogleIcon";
+import Image from "next/image";
+import logo  from "@/app/insuco.png"
 
 export default function OauthButtons() {
 	return (
-		<div className="mt-3 flex items-center justify-between gap-2">
+		<div className="mb-3 flex items-center justify-between gap-2">
 			<OauthButton provider="google" onClick={GoogleLogin}>
-				  <GoogleIcon className="w-5 h-5 mr-2" />
-  					Continue with Google
-
+				  <Image src={logo} alt="App icon" width={20} height={20} className="mr-2" />
+  					Continue with Insuco Workspace
 			</OauthButton>
 		</div>
 	);
